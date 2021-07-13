4b5156be7a569d2857044b256f843f16a2677375
Shen Lan <xz@onwalk.net>
2021-07-13 15:32:15 +0800

    Add axios get example codes

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        This is Vue About Page !
+        {{ info }}
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "Home"
+    name: "Home",
+    data() {
+            return {
+                info: null
+         }
+    },
+    mounted () {
+        axios.get('http://dev.onwalk.net:8000/v1/hello')
+        .then( response => { 
+		this.info = response.data.message;
+                console.log(response);
+	}).catch( error => { 
+		console.log(error);
+	});
+    }
 }
 </script>
 
