9b7568946fa0cac14553f8cdd1bfa1e070f562b2
Shen Lan <xz@onwalk.net>
2021-07-13 16:01:13 +0800

    add axios post example codes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        This is Vue Home Page !
+        {{ info }}
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import qs from 'qs'
+
 export default {
-    name: "Home"
+    name: "Home",
+    data() {
+            return {
+                info: null
+         }
+    },
+    mounted () {
+        let PostData = {
+          message:'TestGinPostAPI'
+        }
+        let data = qs.stringify(PostData);
+        axios.post('http://dev.onwalk.net:8000/v1/post', data)
+        .then( response => {
+                this.info = response.data.status;
+                console.log(response);
+        }).catch( error => {
+                console.log(error);
+        });
+    }
 }
 </script>
 
