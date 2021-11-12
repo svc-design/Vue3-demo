fb9595952673db7699d8425f4e112786f1c0126f
root <user.name>
2021-11-13 00:59:12 +0800

    update components: layout.vue sider.vue

--- src/components/sider.vue
+++ src/components/sider.vue
@@ -1,10 +1,9 @@
 <template>
 <a-menu @click="navClick"
-                style="width: 256px"
                 v-model="currentSelectChild"
                 @openChange="onOpenChange"
                 :openKeys="currentParent"
-                theme="dark"
+                theme="light"
                 mode="inline">
             <!--横向：horizontal-->
             <!--有openKeys时 :defaultOpenKeys="[200]"无效 -->
@@ -24,52 +23,108 @@
 </template>
 
 <script setup>
-export default 
-{
-  data () 
-  {
-    return 
-    {
-       NavData: [
+
+import { ref } from 'vue'
+
+const NavData = [
             {
                 NavID: 100,
                 Icons:"dashboard",
-                Title:"操作",
+                Title:"集群",
                 Path:"",
                 Child: [
                     {
                         NavID: 101,
                         Icons: "",
-                        Title: "用户",
+                        Title: "添加集群",
                         Path: "/UserInfo",
                     }, {
                         NavID: 102,
                         Icons: "",
-                        Title: "公司",
+                        Title: "集群联邦",
                         Path: "/CompanyInfo",
                     },{
                         NavID: 103,
                         Icons: "",
-                        Title: "部门",
+                        Title: "删除集群",
                         Path: "/TeamInfo",
                     },
 
                 ]
             },
             {
+                NavID: 200,
+                Icons:"audit",
+                Title:"网络",
+                Path:"",
+                Child: [{
+                        NavID: 201,
+                        Icons: "",
+                        Title: "服务均衡",
+                        Path: "/Card",
+                    }]
+            },
+            {
                 NavID: 300,
                 Icons:"audit",
-                Title:"报表",
+                Title:"存储",
                 Path:"",
                 Child: [{
                         NavID: 301,
                         Icons: "",
-                        Title: "打卡",
+                        Title: "接入存储",
+                        Path: "/Card",
+                    }]
+            },
+            {
+                NavID: 400,
+                Icons:"audit",
+                Title:"可用性观测",
+                Path:"",
+                Child: [
+                    {
+                        NavID: 401,
+                        Icons: "",
+                        Title: "日志指标",
+                        Path: "/Card",
+                    },
+                    {
+                        NavID: 402,
+                        Icons: "",
+                        Title: "监控指标",
+                        Path: "/Card",
+                    },
+                    {
+                        NavID: 403,
+                        Icons: "",
+                        Title: "链路请求",
+                        Path: "/Card",
+                    }]
+            },
+            {
+                NavID: 500,
+                Icons:"audit",
+                Title:"性能观测",
+                Path:"",
+                Child: [
+                    {
+                        NavID: 501,
+                        Icons: "",
+                        Title: "计算资源",
+                        Path: "/Card",
+                    },
+                    {
+                        NavID: 502,
+                        Icons: "",
+                        Title: "存储资源",
+                        Path: "/Card",
+                    },
+                    {
+                        NavID: 503,
+                        Icons: "",
+                        Title: "网络资源",
                         Path: "/Card",
                     }]
             }
          ]
-      }
-   }
-}
 </script>
