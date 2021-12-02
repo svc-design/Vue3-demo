50d71dd95371645642b256d1be784fc4cae049dc
Haitao Pan <xz@onwalk.net>
2021-12-02 22:48:47 +0800

    vuex-demo: add moduleA getters example

--- vuex-demo/src/App.vue
+++ vuex-demo/src/App.vue
@@ -14,8 +14,19 @@
    <p>computed->count {{ get_vaule_count }}</p>
  </div>
 
+  <div>
+    <p>getters->name {{ getters_name }}</p>
+    <p>getters->count {{ getters_count }}</p>
+  </div>
+
+  <div>
+    <p>modules->name {{ modules_name }}</p>
+  </div>
+
    <button @click='add'> add+1 </button> 
-   <button @click='adds'> add+10 </button> 
+   <button @click='adds'> add+10 </button>   
+   <button @click='sync_adds'> sync_add+10 </button>   
+
 </template>
 
 <script>
@@ -34,7 +45,19 @@ export default {
    },
    get_vaule_count() {
      return this.$store.state.count   
-   } 
+   }, 
+   getters_name() {
+     console.log(this.$store)
+     return this.$store.getters.name  
+   },
+   getters_count() {
+     console.log(this.$store)
+     return this.$store.getters.count  
+   },
+   modules_name() {
+     console.log(this.$store)
+     return this.$store.state.a.name  
+   }
   },
   methods: {
     ...mapMutations([
@@ -42,8 +65,11 @@ export default {
      ]), 
     adds(){
       this.$store.commit('adds',10)
-    }
+    },
+    sync_adds(){
+      this.$store.dispatch('async_commit_adds', { num: 10 } )
   }
+ }
 } 
 </script>
 
--- vuex-demo/src/store.js
+++ vuex-demo/src/store.js
@@ -1,5 +1,16 @@
 import { createStore } from 'vuex'
 
+const moduleA = {
+  namespaced: true, // 需要开启命名空间 识别这个module
+  state: () => ({
+    name: '麦克斯',
+    sex: '6'
+  }),
+  mutations: {},
+  actions: {},
+  getters: {}
+}
+
 const store = createStore ({
     state: {
 	    name: 'global_state',
@@ -23,7 +34,18 @@ const store = createStore ({
 	async_commit_adds({ commit }, data) {
 	    commit('adds', data.num)
 	}
-    }
+    },
+    getters: {
+      name: state => {
+          return state.name
+      },
+      count: state => {
+          return state.count
+      }
+   },
+   modules: {
+     a: moduleA
+   }
 })
 
 export default store
